440b93c7af68e534d222ac81b892a0ef9734ff22
Tim Summers <timsummers@mac.com>
2020-11-18 16:49:00 +0100

    Package for Git
    Ready to be uploaded and packaged from Git

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+
+Seutp for partita package
+
+"""
+
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+      name='harmonypartition',
+      version='0.0.1',
+      author="Timothy Summers",
+      author_email="timsummers.dev@gmail.com",
+      description='Analyze harmonic content of binary pitch-class data',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/timsum/harmonypartition",
+      packages=setuptools.find_packages(),
+      classifiers = ["Development Status :: 3 - Alpha",
+                     "Framework :: Matplotlib",
+                     "Intended Audience :: Education",
+                     "Intended Audience :: Science/Research",
+                     "Intended Audience :: End Users/Desktop",
+                     "License :: OSI Approved :: MIT License",
+                     "Programming Language :: Python :: 3.6",
+                     "Topic :: Games/Entertainment",
+                     "Topic :: Multimedia",
+                     "Topic :: Multimedia :: Sound/Audio :: Analysis",
+                     "Topic :: Multimedia :: Sound/Audio :: MIDI",
+                     "Topic :: Scientific/Engineering :: Visualization"],
+      py_modules=["harmony_state", "partita", "pt_datafiles", "pt_keypattern", "pt_kpdve_list_optimize", "pt_musicutils", "pt_naming_conventions", "pt_utils"],
+)
