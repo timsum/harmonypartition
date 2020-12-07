db4091aad1bf987ee653e60266b6ac41812b1ec0
Tim Summers <timsummers@mac.com>
2020-12-07 14:35:45 +0100

    Update harmony_state.py
    put function current_chord_as_scale_degrees(self) as a way of accessing 7-tone mode combinations.

--- src/harmony_state.py
+++ src/harmony_state.py
@@ -132,8 +132,9 @@ class harmony_state():
     
     # FOR NAVIGATING WITHIN A SEVEN-TONE SCALE ENVIRONMENT
     # assumes seven=tone basis (kp established in scale)
-    def dve_degree_list(self):
+    def current_chord_as_scale_degrees(self):
         chord = [pt_keypattern.DVE_linear_eq(self.current_kpdve[2], self.current_kpdve[3], i for i in range(self.current_kpdve[4] + 1))]
+        chord = (chord * kpdve[3]) % 7
         return np.array(chord)
     
     # --------------------------------------------------------------
