8bd01377166cb81a4239d4a9561ca6940f032839
Tim Summers <timsummers@mac.com>
2020-12-19 16:34:24 +0100

    midianalyzer notebook - student updates from wavewriter
    wavewriter is now set to make files for sequences
    
    elementary work on midianalyzer. setup should work in Jupyter and in terminal

--- jup/partita_music21.py
+++ jup/partita_music21.py
@@ -16,14 +16,18 @@ import harmony_state
 # BARE=BONES integration with music 21
 
 # =============================
-def analyze_parsed_notation_file(parsedfile, key_orientation=np.array([0,0,0,4,2])):
+def analyze_parsed_notation_file(parsedfile, key_orientation=np.array([0,0,0,4,2]), beats_per_slice=0.25):
     '''
-    Analyze an xml file, and send back a tuple of bin_analysis, kpdve_analysis
+    Analyze an xml or midi file, and send back a tuple of bin_analysis, kpdve_analysis
 
     Parameters
     ----------
     filename : str
         the name of a file in the Music21 corpus.
+    key_orientation: np.array(5)
+        the starting point for harmony analysis
+    beats_per_slice : int
+        how often to perform analysis, metrically
 
     Returns
     -------
@@ -36,7 +40,7 @@ def analyze_parsed_notation_file(parsedfile, key_orientation=np.array([0,0,0,4,2
     pitch_classes = []
     for a_chord in s.recurse().getElementsByClass('Chord'):
         # senthentically reduce to sixteenths (if necessary)
-        sixteenths = int(a_chord.quarterLength // 0.25)
+        sixteenths = int(a_chord.quarterLength // beats_per_slice)
         for _ in range(sixteenths):
             pitch_classes.append([])
             for pc in a_chord.pitches:
@@ -55,9 +59,9 @@ def analyze_parsed_notation_file(parsedfile, key_orientation=np.array([0,0,0,4,2
 
     return bin_analysis, np.array(kpdve_chroma)
 
-def analyze_notation_file(filename, key_orientation=np.array([0,0,0,4,2])):
+def analyze_notation_file(filename, key_orientation=np.array([0,0,0,4,2]), beats_per_slice=0.25):
     parsedfile = music21.converter.parse(filename)
-    bin_a, kpdve_a = analyze_parsed_notation_file(parsedfile, key_orientation)
+    bin_a, kpdve_a = analyze_parsed_notation_file(parsedfile, key_orientation, beats_per_slice)
 
     return bin_a, kpdve_a
 
